603aed15a2acac2580ce3a9bd715cca9cf20e730
sanjeev <79185589+sanjeev-kumar-nsut@users.noreply.github.com>
2023-04-23 19:19:39 +0530

    all practicals

---
+++ practical 7/server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const app = express();
+const port = 5000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("studentData.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  } else console.log("Connected to the database");
+});
+// db.run(
+//   "CREATE TABLE studentData (fName varchar(20),lName varchar(20),rNo varchar(20),mNo varchar(20),eMail varchar(20),birthday varchar(20),branch varchar(20),gender varchar(10))"
+// );
+// db.run(`drop table studentData`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", async (req, res) => {
+  let formData = req.body.formData;
+  console.log("received data :", formData);
+  let sql =
+    "INSERT INTO studentData VALUES(" +
+    formData.fName +
+    "," +
+    formData.lName +
+    "," +
+    formData.rNo +
+    "," +
+    formData.mNo +
+    "," +
+    formData.eMail +
+    "," +
+    formData.birthday +
+    "," +
+    formData.branch +
+    "," +
+    formData.gender +
+    "," +
+    formData.fileInput +
+    "," +
+    formData.description +
+    ")";
+  let result = await db.run(sql);
+  console.log("result :", result);
+  res.send(result.acknowledgement);
+});
+
+app.listen(port, async () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ practical 8/server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 5000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const mongodb_url = "mongodb://localhost:27017";
+const client = new MongoClient(mongodb_url);
+
+// Database Name
+const dbName = "webTech";
+
+app.get("/", async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+  const data = await collection.find({}).toArray();
+  console.log(" result :", data);
+  res.send(data);
+});
+app.post("/", async (req, res) => {
+  let formData = req.body.formData;
+  console.log("received data :", formData);
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+  const insertResult = await collection.insertOne(formData);
+  console.log("insert result :", insertResult);
+  res.send(insertResult.acknowledged);
+});
+
+app.listen(port, async () => {
+  console.log(`Example app listening on port ${port}`);
+
+  await client.connect();
+  console.log("Connected successfully to server");
+});
